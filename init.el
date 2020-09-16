61df005e00b0831a957031415b6bd24dffaaa833
Sungmin Yang <kimchhickey@gmail.com>
2020-09-16 15:12:20 +0900

    change backup folders and haskell-mode

--- init.el
+++ init.el
@@ -19,3 +19,16 @@
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/my-emacs-config.org"))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+	 '(haskell-mode company cider clojure-mode-extra-font-locking clojure-mode rainbow-delimiters paredit magit dashboard rainbow-mode projectile smex which-key exec-path-from-shell afternoon-theme use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
